8749aff235bebd7fd9ddcb6b23fc507608a5e38c
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-04-07 12:01:49 +0200

    r wordcloud
    r wordcloud

--- MovieBuffBot/resources/R/wordcloud.R
+++ MovieBuffBot/resources/R/wordcloud.R
@@ -3,10 +3,58 @@
 # install.packages("RMySQL")
 library(RMySQL)
 
+# Install
+#install.packages("tm")  # for text mining
+#install.packages("SnowballC") # for text stemming
+#install.packages("wordcloud") # word-cloud generator 
+#install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
 con <- dbConnect(MySQL(), dbname="imdb", user="imdb", password="imdb")
 values <- dbGetQuery(con, "jdbc localhost 3306 imdb imdb imdb select actors.Actor as actorname, count(roles.ActorID) as countroles from roles, actors, movies, countries where roles.MovieID = movies.MovieID and roles.ActorID = actors.ActorID and countries.MovieID = movies.MovieID and Country ='<star>'")
 
-invisible(jpeg('/tmp/video-format.jpg'))
-barplot(values$freq, names.arg = values$format, horiz=FALSE, cex.names=0.5)
-invisible(dev.off())
+# Load the data as a corpus
+docs <- Corpus(VectorSource(values))
+#text transformation
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+
+#term document matrix
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+#generate word cloud
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+#invisible(jpeg('/tmp/video-format.jpg'))
+#barplot(values$freq, names.arg = values$format, horiz=FALSE, cex.names=0.5)
+#invisible(dev.off())
 

5dd8840b7484cda9ec3281b825d1afabaf0faabe
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-04-08 19:38:02 +0200

    r scripts met plots
    r scripts met plots

--- MovieBuffBot/resources/R/oudeactrices.R
+++ MovieBuffBot/resources/R/oudeactrices.R
@@ -3,10 +3,39 @@
 # install.packages("RMySQL")
 library(RMySQL)
 
+#wiebe R
 con <- dbConnect(MySQL(), dbname="imdb", user="imdb", password="imdb")
 values <- dbGetQuery(con, "select actors.Actor as actorname, count(roles.ActorID) as countroles, movies.Year as movieyear from roles, actors, movies where roles.MovieID = movies.MovieID and roles.ActorID = actors.ActorID and actors.Gender ='1'")
 
-invisible(jpeg('/tmp/video-format.jpg'))
-barplot(values$freq, names.arg = values$format, horiz=FALSE, cex.names=0.5)
+#model
+model <-glm(serierating~countroles+movieyear,data=values,family=binomial)
+summary(model)
+
+#prediction
+predictTest <-predict(model,type="response")
+
+#library roc
+library(ROCR)
+ROCRperfi <-performance(predictTest,"tpr","fpr")
+
+#ROC plot
+invisible(jpeg('/tmp/oudeactrices.jpg'))
+plot(ROCRperfi,colorize=true,print.cutoffs.at=seq(0,1,0.1))
 invisible(dev.off())
+abline(0,1)
+
+#AUC
+AUC <-as.numeric(performance(predictTelco,”AUC”)@y.values)
+str(AUC)
+
+#confusion matrix function
+confusion <-function(arg1,arg2,arg3){
+table = table(arg1,arg2>arg3)
+specificity <-table[1,1]/(table[1,1]+table[1,2])
+sensitivity <-table[2,2]/(table[2,1]+table[2,2])
+accuracy <-(table[1,1]+table[2,2])/(table[1,1]+table[1,2]+table[2,1]+table[2,2])
+return(data.frame(Specificity=specificity,Sensitivity=sensitivity,Accuracy=accuracy))}
+
+#show confusion
+confusion(model$serierating,predictTest,0.1)
 
